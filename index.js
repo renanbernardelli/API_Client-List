3d3b1442dee883fdd2fead5887fecc85a68feb1d
Renan Bernardelli <renanbernardelli@gmail.com>
2020-03-22 10:24:03 +0000

    :point_right: Create route to list all clients from DB

--- index.js
+++ index.js
@@ -17,3 +17,35 @@ app.use('/', router);
 //inicia o servidor
 app.listen(port);
 console.log('API funcionando!');
+
+//criar rota para listar todos os clientes do DB
+function execSQLQuery(sqlQry, res) {
+    
+    const connection = mysql.createConnection({
+
+        host: 'XXX',
+        port: 'XXX',
+        user: 'XXX',
+        password: 'XXX',
+        database: 'XXX'
+    });
+
+    connection.query(sqlQry, function(error, results, fields) {
+        
+        if (error) {
+            res.json(error);
+        }
+        else {
+            res.json(results);
+        }
+
+        connection.end();
+        console.log('executou!');
+    });
+};
+
+//criar rota /clients
+router.get('/clients', (req, res) => {
+
+    execSQLQuery('SELECT * FROM Client_List', res);
+})
